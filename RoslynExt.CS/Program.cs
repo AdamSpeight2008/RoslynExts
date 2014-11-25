c646314ab6db407d79a96ff890b48fc655c90a17
AdamSpeight2008 <Adam.Speight@virgin.net>
2014-11-25 20:45:12 +0000

    Initial Commit

---
+++ RoslynExt.CS/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace DotNetAnalyzers.RoslynExts.CS
+{
+  static class Exts
+  {
+    static MethodDeclarationSyntax InWhichMethod(SyntaxNode sn)
+    {
+      return sn.FirstAncestorOrSelf<MethodDeclarationSyntax>();
+    }
+     
+    static TypeDeclarationSyntax InWhichTypeDeclaration(SyntaxNode sn)
+    {
+      return sn.FirstAncestorOrSelf<TypeDeclarationSyntax>();
+    }
+
+    static T ToExpr<T>(this string code) where T : ExpressionSyntax
+    {
+      return SyntaxFactory.ParseExpression(code) as T;
+    }
+
+    static T ToSExpr<T>(this string code) where T : StatementSyntax
+    {
+      return SyntaxFactory.ParseStatement(code) as T;
+    }
+  }
+}
---
+++ RoslynExts/Module1.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Runtime.CompilerServices
+
+Namespace Global.DotNetAnalyzers.RoslynExts
+  Namespace VB
+    <HideModuleName()>
+    Public Module Exts
+
+      <Extension>
+      Public Function InWhichMethod(sn As SyntaxNode) As MethodBaseSyntax
+        Return sn.FirstAncestorOrSelf(Of MethodBaseSyntax)
+      End Function
+
+      <Extension>
+      Public Function InWhichTypeDeclaration(sn As SyntaxNode) As TypeBlockSyntax
+        Return sn.FirstAncestorOrSelf(Of TypeBlockSyntax)
+      End Function
+
+      <Extension>
+      Public Function ToExpr(Of T As ExpressionSyntax)(code As String) As T
+        Return DirectCast(SyntaxFactory.ParseExpression(code), T)
+      End Function
+
+      <Extension>
+      Public Function ToSExpr(Of T As StatementSyntax)(code As String) As T
+        Return DirectCast(SyntaxFactory.ParseExecutableStatement(code), T)
+      End Function
+
+    End Module
+
+  End Namespace
+
+End Namespace
